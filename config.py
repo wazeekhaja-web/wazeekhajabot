15ba818008f422009d9d5ae9c67bae109b0b3a65
wazeekhaja-web <wazeekhaja0@gmail.com>
2025-10-18 06:59:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ SHORT_TUT = "https://t.me/How_to_Download_7x/26"
 
 # Bot Configuration
 SESSION = "yato"
-TOKEN = "642712"
+TOKEN = "8495837800:AAGycktDit_T539eHme1OSofok3tUrjfTJg"
 API_ID = "27642526"
 API_HASH = "8bc14441805c29b64843165c1d73ce31"
 WORKERS = 5
